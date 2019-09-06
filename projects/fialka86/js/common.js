db347fdad44d62c74bb83a8e2968c449cca3b529
kve1989 <kve1989@ya.ru>
2019-09-06 16:48:39 +0500

    Fix proj

--- projects/fialka86/js/common.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('.slider').slick({
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 10000,
-    prevArrow: $('.arrow-prev'),
-    nextArrow: $('.arrow-next')
-  });
-});
