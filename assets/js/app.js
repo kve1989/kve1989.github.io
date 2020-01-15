31dc3f8b00648e9f183d3dceee6ecb4ccf557a25
Вадим Евгеньевич Кавецкий <KVE@okbhmao.local>
2020-01-15 14:20:54 +0500

    Fox

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
-const w = window.outerWidth;
-const h = window.outerHeight;
-const scrWidth = document.querySelector('.screensize-w');
-const scrHeight = document.querySelector('.screensize-h');
-const appBrowser = document.querySelector('.browser');
-
-scrWidth.children.textContent = w;
-scrHeight.children.textContent = h;
+document.addEventListener('DOMContentLoaded', () => {
+	const w = window.outerWidth;
+	const h = window.outerHeight;
+	const scrWidth = document.querySelector('.screensize-w');
+	const scrHeight = document.querySelector('.screensize-h');
+	const appBrowser = document.querySelector('.browser');
+	
+	scrWidth.children.textContent = w;
+	scrHeight.children.textContent = h;
+});
