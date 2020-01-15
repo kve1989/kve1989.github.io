db8214fd4773675218b5e9ea7cbcbcc93f9648f6
Вадим Евгеньевич Кавецкий <KVE@okbhmao.local>
2020-01-15 13:42:33 +0500

    Fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 const w = window.outerWidth;
 const h = window.outerHeight;
+const scrWidth = document.querySelector('.screensize-w');
+const scrHeight = document.querySelector('.screensize-h');
+const appBrowser = document.querySelector('.browser');
 
-console.log(w, h);
+scrWidth.children.textContent = w;
+scrHeight.children.textContent = h;
