558272929c6e3c7bfb4e9b2592d68cd8532ceb8d
kve1989 <kve1989@ya.ru>
2020-02-04 22:10:00 +0500

    Fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-	const w = window.outerWidth;
-	const h = window.outerHeight;
 
 	let scrSizeW = document.querySelector('.screensize-w span');
 	let scrSizeH = document.querySelector('.screensize-h span');
 	let browser = document.querySelector('.browser span');
 
-	scrSizeW.textContent = w;
-	scrSizeH.textContent = h;
 	browser.textContent = window.navigator.userAgent;
+
+	window.addEventListener('resize', function(event) {
+		let w = window.outerWidth;
+		let h = window.outerHeight;
+
+		scrSizeW.textContent = w;
+		scrSizeH.textContent = h;
+
+	});
 });
