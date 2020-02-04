5b80038071d55f43ada944684a509bae9ec9841e
kve1989 <kve1989@ya.ru>
2020-02-04 21:37:27 +0500

    Fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+
 	const w = window.outerWidth;
 	const h = window.outerHeight;
-	const scrWidth = document.querySelector('.screensize-w');
-	const scrHeight = document.querySelector('.screensize-h');
-	const appBrowser = document.querySelector('.browser');
-	
-	scrWidth.children.textContent = w;
-	scrHeight.children.textContent = h;
-	window.ou
+
+	let scrSizeW = document.querySelector('.screensize-w span');
+	let scrSizeH = document.querySelector('.screensize-h span');
+	let browser = document.querySelector('.browser span');
+
+	scrSizeW.textContent = w;
+	scrSizeH.textContent = h;
+	browser.textContent = window.navigator.userAgent;
 });
