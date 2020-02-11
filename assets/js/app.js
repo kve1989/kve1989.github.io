bea857ee567818adbf4f4f98a82b2aa99532fd46
Вадим Евгеньевич Кавецкий <KVE@okbhmao.local>
2020-02-11 13:55:05 +0500

    Fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,33 @@ document.addEventListener('DOMContentLoaded', () => {
 		scrSizeW.textContent = window.outerWidth;
 		scrSizeH.textContent = window.outerHeight;
 	});
+
+	let isMobile = {
+		Android: function () {
+			return navigator.userAgent.match(/Android/i);
+		},
+		BlackBerry: function () {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+		iOS: function () {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function () {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+		Windows: function () {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+		any: function () {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+	let body = document.querySelector('body');
+
+	if (isMobile.any()) {
+		body.classList.add('touch');
+	} else {
+		body.classList.add('mouse');
+	}
 });
