7469dc28d5d872e49e6a69de15453d64ff0de6e3
Вадим Евгеньевич Кавецкий <KVE@okbhmao.local>
2020-05-07 11:18:44 +0500

    add animation for show item

--- _in_process/WorldPrint/app/#src/js/modules/showMore.js
+++ _in_process/WorldPrint/app/#src/js/modules/showMore.js
@@ -5,7 +5,8 @@ const showMore = (trigger, wrapper) => {
 	btn.addEventListener('click', () => {
 		wrappers.forEach(wrap => {
 			if (wrap.classList.contains('hidden')) {
-				wrap.classList.remove('hidden')
+				wrap.classList.remove('hidden');
+				wrap.classList.add('animated', 'slideInUp');
 			}
 		});
 		btn.remove();
