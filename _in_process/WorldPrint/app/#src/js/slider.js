4f92e94a4759609b1b64c8a86d78f572388acfa9
kve1989 <kve1989@ya.ru>
2020-05-07 21:45:07 +0500

    Fix

--- _in_process/WorldPrint/app/#src/js/slider.js
+++ _in_process/WorldPrint/app/#src/js/slider.js
@@ -26,4 +26,41 @@ $(function() {
 		}
 	}]
 	});
+
+	var section = $('.section'),
+		nav = $('.aside__lists'),
+		navHeight = nav.outerHeight(); // получаем высоту навигации
+
+
+	  // поворот экрана
+	  window.addEventListener('orientationchange', function () {
+	  	navHeight = nav.outerHeight();
+	  }, false);
+
+	  $(window).on('scroll', function () {
+	  	const position = $(this).scrollTop();
+
+	  	section.each(function () {
+	  		const top = $(this).offset().top - navHeight - 5,
+	  			bottom = top + $(this).outerHeight();
+
+	  		if (position >= top && position <= bottom) {
+	  			nav.find('a').removeClass('active');
+	  			section.removeClass('active');
+
+	  			$(this).addClass('active');
+	  			nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+	  		}
+	  	});
+	  });
+
+	  nav.find('a').on('click', function () {
+	  	const id = $(this).attr('href');
+
+	  	$('html, body').animate({
+	  		scrollTop: $(id).offset().top - navHeight
+	  	}, 500);
+
+	  	return false;
+	  });
 });
