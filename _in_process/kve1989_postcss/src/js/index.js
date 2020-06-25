435851cb1f684919c76a567fc534e785b5aa6b9a
kve1989 <kve1989@ya.ru>
2020-06-25 22:35:36 +0500

    Change css

--- _in_process/kve1989_postcss/src/js/index.js
+++ _in_process/kve1989_postcss/src/js/index.js
@@ -1,4 +1,9 @@
+import isMobile from './modules/isMobile';
+
 document.addEventListener("DOMContentLoaded", () => {
+
+  isMobile();
+
   let scrSizeW = document.querySelector(".screensize-w");
   let scrSizeH = document.querySelector(".screensize-h");
   let browser = document.querySelector(".browser span");
@@ -12,38 +17,5 @@ document.addEventListener("DOMContentLoaded", () => {
     scrSizeH.textContent = window.outerHeight;
   });
 
-  let isMobile = {
-    Android: function () {
-      return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-      return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-      return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-      return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-      return (
-        isMobile.Android() ||
-        isMobile.BlackBerry() ||
-        isMobile.iOS() ||
-        isMobile.Opera() ||
-        isMobile.Windows()
-      );
-    }
-  };
 
-  let body = document.querySelector("body");
-
-  if (isMobile.any()) {
-    body.classList.add("touch");
-  } else {
-    body.classList.add("mouse");
-  }
 });
