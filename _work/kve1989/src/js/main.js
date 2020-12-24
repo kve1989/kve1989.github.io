2177a659a4cddf204c7484ed47dfbbb96b58f72f
kve1989 <kve1989@ya.ru>
2020-12-24 23:06:54 +0500

    Реализовано сохранение сессия для темы

--- _work/kve1989/src/js/main.js
+++ _work/kve1989/src/js/main.js
@@ -1,27 +1,40 @@
-import isMobile from './modules/isMobile';
-import typeText from './modules/typeText';
-import scrolling from './modules/scrolling';
+import isMobile from "./modules/isMobile";
+import typeText from "./modules/typeText";
+import scrolling from "./modules/scrolling";
 
 document.addEventListener("DOMContentLoaded", () => {
+	isMobile();
+	typeText(["VKavetskiy"], ".header__logo");
+	scrolling();
 
-  isMobile();
-  typeText(["VKavetskiy"], '.header__logo');
-  scrolling();
+	let scrSizeW = document.querySelector(".screensize-w");
+	let scrSizeH = document.querySelector(".screensize-h");
+	let browser = document.querySelector(".browser span");
+	const switchTheme = document.querySelector(".switch__input");
 
-  let scrSizeW = document.querySelector(".screensize-w");
-  let scrSizeH = document.querySelector(".screensize-h");
-  let browser = document.querySelector(".browser span");
+	if (sessionStorage.getItem("DarkThemeActive")) {
+		switchTheme.checked = true;
+		document.querySelector(".page").classList.add("page--night");
+	}
 
-  scrSizeW.textContent = window.outerWidth;
-  scrSizeH.textContent = window.outerHeight;
-  browser.textContent = window.navigator.userAgent;
+	switchTheme.addEventListener("change", (e) => {
+		if (e.target.checked) {
+			sessionStorage.setItem("DarkThemeActive", true);
+		} else {
+			sessionStorage.setItem("DarkThemeActive", false);
+		}
+	});
 
-  window.addEventListener("resize", function (event) {
-    scrSizeW.textContent = window.outerWidth;
-    scrSizeH.textContent = window.outerHeight;
-  });
+	scrSizeW.textContent = window.outerWidth;
+	scrSizeH.textContent = window.outerHeight;
+	browser.textContent = window.navigator.userAgent;
 
-  document.querySelector(".switch__input").addEventListener('change', () => {
-    document.querySelector(".page").classList.toggle("page--night");
-  });
+	window.addEventListener("resize", function (event) {
+		scrSizeW.textContent = window.outerWidth;
+		scrSizeH.textContent = window.outerHeight;
+	});
+
+	document.querySelector(".switch__input").addEventListener("change", () => {
+		document.querySelector(".page").classList.toggle("page--night");
+	});
 });
