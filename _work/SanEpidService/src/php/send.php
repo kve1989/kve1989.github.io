9d948dc62b7f7518fefe4a8d04e551b10af79823
kve1989 <kve1989@ya.ru>
2021-01-17 21:03:20 +0500

    2 проекта отправлены в архив

--- _work/SanEpidService/src/php/send.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$to = "cec-info@mail.ru";
-$subject = "Заказ с сайта";
-$message = "Имя: $name\nТелефон: $phone";
-mail ($to,$subject,$message,"Content-type:text/plain;charset = utf-8") or print "Не могу отправить письмо !!!";
-?>
