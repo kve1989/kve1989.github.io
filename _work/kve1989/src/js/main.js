85d1b70f6ed172bd75a2daa10f84107a3aa6b87f
kve1989 <kve1989@ya.ru>
2021-02-18 20:35:15 +0500

    remove customize because mv in branche

--- _work/kve1989/src/js/main.js
+++ _work/kve1989/src/js/main.js
@@ -15,10 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		scrSizeH = document.querySelector(".screensize-h"),
 		browser = document.querySelector(".browser span"),
 		switchTheme = document.querySelector(".switch__input"),
-		customSwitch = document.querySelector(".customize__head"),
-		inputBg = document.getElementById("colorBg"),
-		inputAccent = document.getElementById("colorAccent"),
-		inputFont = document.getElementById("colorFont");
+		customSwitch = document.querySelector(".customize__head");
 
 	if (
 		sessionStorage.getItem("DarkThemeActive") &&
@@ -47,70 +44,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	switchTheme.addEventListener("change", () => {
 		document.querySelector(".page").classList.toggle("page--night");
-		if (document.querySelector(".page").closest("page--night")) {
-			inputBg.value = getComputedStyle(
-				document.querySelector(".page--night")
-			)
-				.getPropertyValue("--bg-color")
-				.trim();
-
-			inputFont.value = getComputedStyle(
-				document.querySelector(".page--night")
-			)
-				.getPropertyValue("--font-color")
-				.trim();
-
-			inputAccent.value = getComputedStyle(
-				document.querySelector(".page")
-			)
-				.getPropertyValue("--accent-color")
-				.trim();
-		}
-		inputBg.value = getComputedStyle(document.querySelector(".page"))
-			.getPropertyValue("--bg-color")
-			.trim();
-
-		inputFont.value = getComputedStyle(document.querySelector(".page"))
-			.getPropertyValue("--font-color")
-			.trim();
-
-		inputAccent.value = getComputedStyle(document.querySelector(".page"))
-			.getPropertyValue("--accent-color")
-			.trim();
 	});
 
 	customSwitch.addEventListener("click", (e) => {
 		e.target.parentNode.classList.toggle("active");
 		e.target.classList.toggle("active");
-	});
-
-	inputBg.value = getComputedStyle(document.querySelector(".page"))
-		.getPropertyValue("--bg-color")
-		.trim();
-
-	inputFont.value = getComputedStyle(document.querySelector(".page"))
-		.getPropertyValue("--font-color")
-		.trim();
-
-	inputAccent.value = getComputedStyle(document.querySelector(".page"))
-		.getPropertyValue("--accent-color")
-		.trim();
-
-	inputBg.addEventListener("input", (e) => {
-		document
-			.querySelector(".page")
-			.style.setProperty("--bg-color", e.target.value);
-	});
-
-	inputFont.addEventListener("input", (e) => {
-		document
-			.querySelector(".page")
-			.style.setProperty("--font-color", e.target.value);
-	});
-
-	inputAccent.addEventListener("input", (e) => {
-		document
-			.querySelector(".page")
-			.style.setProperty("--accent-color", e.target.value);
 	});
 });
