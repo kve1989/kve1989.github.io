0eb37ad539ca0b319c360eab058e900784c0dee4
kve1989 <kve1989@ya.ru>
2022-04-12 17:01:23 +0500

    Update package sass version

--- _work/kve1989/gulpfile.js
+++ _work/kve1989/gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require('gulp-sass')(require('sass'));
 const sassglob = require("gulp-sass-glob");
 const browserSync = require("browser-sync");
 const rename = require("gulp-rename");
